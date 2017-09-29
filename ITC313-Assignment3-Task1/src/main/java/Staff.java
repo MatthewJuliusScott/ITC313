9a87b8fdc49b70ed0964c33a0cda27af1866bd58
Matthew <matthewjuliusscott@gmail.com>
2017-09-29 21:53:28 +1000

    finished task 1

--- ITC313-Assignment3-Task1/src/main/java/Staff.java
+++ ITC313-Assignment3-Task1/src/main/java/Staff.java
@@ -2,20 +2,118 @@
 
 public class Staff {
 	
-	private int id;
+	private int id = 0;
+	
+	private String lastName;
+	
+	private String firstName;
+	
+	private String middleInitial;
+	
+	private String address;
+	
+	private String city;
+	
+	private String state;
+	
+	private String telephoneNumber;
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Staff)) {
+			return false;
+		}
+		Staff other = (Staff) obj;
+		if (address == null) {
+			if (other.address != null) {
+				return false;
+			}
+		} else if (!address.equals(other.address)) {
+			return false;
+		}
+		if (city == null) {
+			if (other.city != null) {
+				return false;
+			}
+		} else if (!city.equals(other.city)) {
+			return false;
+		}
+		if (firstName == null) {
+			if (other.firstName != null) {
+				return false;
+			}
+		} else if (!firstName.equals(other.firstName)) {
+			return false;
+		}
+		if (id != other.id) {
+			return false;
+		}
+		if (lastName == null) {
+			if (other.lastName != null) {
+				return false;
+			}
+		} else if (!lastName.equals(other.lastName)) {
+			return false;
+		}
+		if (middleInitial == null) {
+			if (other.middleInitial != null) {
+				return false;
+			}
+		} else if (!middleInitial.equals(other.middleInitial)) {
+			return false;
+		}
+		if (state == null) {
+			if (other.state != null) {
+				return false;
+			}
+		} else if (!state.equals(other.state)) {
+			return false;
+		}
+		if (telephoneNumber == null) {
+			if (other.telephoneNumber != null) {
+				return false;
+			}
+		} else if (!telephoneNumber.equals(other.telephoneNumber)) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
 	
 	/**
 	 * @return the id
 	 */
 	public int getId() {
 		return id;
-	}
-	
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(int id) {
-		this.id = id;
 	}
 	
 	/**
@@ -26,66 +124,10 @@ public class Staff {
 	}
 	
 	/**
-	 * @param lastName the lastName to set
-	 */
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	
-	/**
-	 * @return the firstName
-	 */
-	public String getFirstName() {
-		return firstName;
-	}
-	
-	/**
-	 * @param firstName the firstName to set
-	 */
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	
-	/**
 	 * @return the middleInitial
 	 */
 	public String getMiddleInitial() {
 		return middleInitial;
-	}
-	
-	/**
-	 * @param middleInitial the middleInitial to set
-	 */
-	public void setMiddleInitial(String middleInitial) {
-		this.middleInitial = middleInitial;
-	}
-	
-	/**
-	 * @return the address
-	 */
-	public String getAddress() {
-		return address;
-	}
-	
-	/**
-	 * @param address the address to set
-	 */
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	
-	/**
-	 * @return the city
-	 */
-	public String getCity() {
-		return city;
-	}
-	
-	/**
-	 * @param city the city to set
-	 */
-	public void setCity(String city) {
-		this.city = city;
 	}
 	
 	/**
@@ -96,31 +138,92 @@ public class Staff {
 	}
 	
 	/**
-	 * @param state the state to set
-	 */
-	public void setState(String state) {
-		this.state = state;
-	}
-	
-	/**
 	 * @return the telephoneNumber
 	 */
 	public String getTelephoneNumber() {
 		return telephoneNumber;
 	}
 	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result
+		        + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + id;
+		result = prime * result
+		        + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result
+		        + ((middleInitial == null) ? 0 : middleInitial.hashCode());
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		result = prime * result
+		        + ((telephoneNumber == null) ? 0 : telephoneNumber.hashCode());
+		return result;
+	}
+	
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	/**
+	 * @param middleInitial the middleInitial to set
+	 */
+	public void setMiddleInitial(String middleInitial) {
+		this.middleInitial = middleInitial;
+	}
+	/**
+	 * @param state the state to set
+	 */
+	public void setState(String state) {
+		this.state = state;
+	}
 	/**
 	 * @param telephoneNumber the telephoneNumber to set
 	 */
 	public void setTelephoneNumber(String telephoneNumber) {
 		this.telephoneNumber = telephoneNumber;
 	}
-	private String lastName;
-	private String firstName;
-	private String middleInitial;
-	private String address;
-	private String city;
-	private String state;
-	private String telephoneNumber;
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Staff [id=" + id + ", lastName=" + lastName + ", firstName="
+		        + firstName + ", middleInitial=" + middleInitial + ", address="
+		        + address + ", city=" + city + ", state=" + state
+		        + ", telephoneNumber=" + telephoneNumber + "]";
+	}
 
 }

bd28e7490b79da5a743d1ba27b3171560f197b9f
Matthew <matthewjuliusscott@gmail.com>
2017-10-01 16:49:25 +1100

    finished threaded animation

--- ITC313-Assignment3-Task3/src/main/java/BouncingBallAnimation.java
+++ ITC313-Assignment3-Task3/src/main/java/BouncingBallAnimation.java
@@ -1,120 +1,161 @@
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.DoubleProperty;
-import javafx.geometry.Pos;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.event.EventHandler;
 import javafx.scene.Node;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollBar;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.util.Duration;
+import javafx.stage.Stage;
 
 public class BouncingBallAnimation extends Application {
-  @Override // Override the start method in the Application class
-  public void start(Stage primaryStage) {
-    BallPane ballPane = new BallPane();
-    ballPane.setStyle("-fx-border-color: yellow");
-    ballPane.add();
 
-    // Pause and resume animation
-    ballPane.setOnMousePressed(e -> ballPane.pause());
-    ballPane.setOnMouseReleased(e -> ballPane.play());
+	public BallPane ballPane;
 
-    // Use a scroll bar to control animation speed
-    ScrollBar sbSpeed = new ScrollBar();
-    sbSpeed.setMax(20);
-    sbSpeed.setValue(10);
-    ballPane.rateProperty().bind(sbSpeed.valueProperty());
-    
-    BorderPane pane = new BorderPane();
-    pane.setCenter(ballPane);
-    pane.setTop(sbSpeed);
+	@Override // Override the start method in the Application class
+	public void start(Stage primaryStage) {
 
-    // Create a scene and place the pane in the stage
-    Scene scene = new Scene(pane, 250, 150);
-    primaryStage.setTitle("Bouncing Ball Animation"); // Set the stage title
-    primaryStage.setScene(scene); // Place the scene in the stage
-    primaryStage.show(); // Display the stage
-  }
+		BorderPane pane = new BorderPane();
 
-  private class BallPane extends Pane {
-    private Timeline animation;
+		// Create a scene and place the pane in the stage
+		Scene scene = new Scene(pane, 250, 150);
+		primaryStage.setTitle("Bouncing Ball Animation"); // Set the stage title
+		primaryStage.setScene(scene); // Place the scene in the stage
+		primaryStage.show(); // Display the stage
 
-    public BallPane() {
-      // Create an animation for moving the ball
-      animation = new Timeline(
-        new KeyFrame(Duration.millis(50), e -> moveBall()));
-      animation.setCycleCount(Timeline.INDEFINITE);
-      animation.play(); // Start animation
-    }
+		// increase or decrease the ball velocity with up and down arrow keys
+		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
 
-    public void add() {
-      Color color = new Color(Math.random(), 
-        Math.random(), Math.random(), 0.5);
-      getChildren().add(new Ball(30, 30, 20, color)); 
-    }
-    
-    public void play() {
-      animation.play();
-    }
+			@Override
+			public void handle(KeyEvent event) {
+				switch (event.getCode()) {
+					case UP :
+						ballPane.increaseSpeed();
+						break;
+					case DOWN :
+						ballPane.decreaseSpeed();
+						break;
+					default :
+						break;
+				}
+			}
+		});
 
-    public void pause() {
-      animation.pause();
-    }
+		ballPane = new BallPane();
+		ballPane.add();
 
-    public void increaseSpeed() {
-      animation.setRate(animation.getRate() + 0.1);
-    }
+		// Create speed label
+		Label speedLabel = new Label("");
+		speedLabel.textProperty().bind(Bindings.concat("Speed: ")
+		        .concat(ballPane.rateProperty().asString()));
+		pane.setCenter(ballPane);
+		pane.setTop(speedLabel);
 
-    public void decreaseSpeed() {
-      animation.setRate(
-        animation.getRate() > 0 ? animation.getRate() - 0.1 : 0);
-    }
+		// Run the animation in a background thread
+		Thread t = new Thread(new Runnable() {
 
-    public DoubleProperty rateProperty() {
-      return animation.rateProperty();
-    }
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						// Wait for animation interval milliseconds.
+						long interval = (long) (50
+						        / ballPane.rateProperty().doubleValue());
+						if (interval > 500) {
+							interval = 500;
+							Thread.sleep(500);
+							continue;
+						} else if (interval < 1) {
+							interval = 1;
+							Thread.sleep(1);
+						}
+						Thread.sleep(interval);
+					} catch (InterruptedException ex) {
+						ex.printStackTrace();
+					}
+					Platform.runLater(new Runnable() {
 
-    protected void moveBall() {
-      for (Node node: this.getChildren()) {
-        Ball ball = (Ball)node;
-        // Check boundaries
-        if (ball.getCenterX() < ball.getRadius() || 
-            ball.getCenterX() > getWidth() - ball.getRadius()) {
-          ball.dx *= -1; // Change ball move direction
-        }
-        if (ball.getCenterY() < ball.getRadius() || 
-            ball.getCenterY() > getHeight() - ball.getRadius()) {
-          ball.dy *= -1; // Change ball move direction
-        }
+						@Override
+						public void run() {
+							ballPane.moveBall();
+						}
+					});
+				}
+			}
+		}, "Animation Thread");
 
-        // Adjust ball position
-        ball.setCenterX(ball.dx + ball.getCenterX());
-        ball.setCenterY(ball.dy + ball.getCenterY());
-      }
-    }
-  }
+		// Terminate the running thread if the application exits
+		t.setDaemon(true);
+		// Start the thread
+		t.start();
+	}
 
-  class Ball extends Circle {
-    private double dx = 1, dy = 1;
+	public class BallPane extends Pane {
 
-    Ball(double x, double y, double radius, Color color) {
-      super(x, y, radius);
-      setFill(color); // Set ball color
-    }
-  }
-  
-  /**
-   * The main method is only needed for the IDE with limited
-   * JavaFX support. Not needed for running from the command line.
-   */
-  public static void main(String[] args) {
-    launch(args);
-  }
+		DoubleProperty animationRate = new SimpleDoubleProperty();
+
+		public void add() {
+			animationRate.set(1);
+			Color color = new Color(Math.random(), Math.random(), Math.random(),
+			        0.5);
+			getChildren().add(new Ball(30, 30, 20, color));
+		}
+
+		public void increaseSpeed() {
+			double rate = (double) Math.round(animationRate.get() * 10) / 10;
+			animationRate.set(rate + 0.1);
+		}
+
+		public void decreaseSpeed() {
+			double rate = (double) Math.round(animationRate.get() * 10) / 10;
+			animationRate.set(animationRate.get() > 0 ? rate - 0.1 : 0);
+		}
+
+		public DoubleProperty rateProperty() {
+			return animationRate;
+		}
+
+		protected void moveBall() {
+			for (Node node : this.getChildren()) {
+				Ball ball = (Ball) node;
+				// Check boundaries
+				if (ball.getCenterX() < ball.getRadius()
+				        || ball.getCenterX() > getWidth() - ball.getRadius()) {
+					ball.dx *= -1; // Change ball move direction
+				}
+				if (ball.getCenterY() < ball.getRadius()
+				        || ball.getCenterY() > getHeight() - ball.getRadius()) {
+					ball.dy *= -1; // Change ball move direction
+				}
+
+				// Adjust ball position
+				ball.setCenterX(ball.dx + ball.getCenterX());
+				ball.setCenterY(ball.dy + ball.getCenterY());
+			}
+		}
+	}
+
+	class Ball extends Circle {
+
+		private double dx = 1, dy = 1;
+
+		Ball(double x, double y, double radius, Color color) {
+			super(x, y, radius);
+			setFill(color); // Set ball color
+		}
+	}
+
+	/**
+	 * The main method is only needed for the IDE with limited JavaFX support.
+	 * Not needed for running from the command line.
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
